7f53fe5aade4f989e5cb96180d445771c7280027
Hưng Nguyễn Xuân <nxhung16102001@gmail.com>
2021-07-08 11:35:44 +0700

    update product

--- AnimatedColorfulMenu/ViewModel/UpdateProductViewModel.cs
+++ AnimatedColorfulMenu/ViewModel/UpdateProductViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using AnimatedColorfulMenu.Model;
 using AnimatedColorfulMenu.View;
 using Firebase.Storage;
-using Google.Cloud.Firestore;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -9,7 +8,6 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,20 +16,12 @@ using System.Windows.Media.Imaging;
 
 namespace AnimatedColorfulMenu.ViewModel
 {
-    class SanPhamViewModel : MainViewModel
+    class UpdateProductViewModel : BaseViewModel
     {
 
         public ICommand themProduct { get; set; }
 
         public ICommand chonHinhAnh { get; set; }
-
-        public ICommand xoaProduct { get; set; }
-
-        public ICommand suaProduct { get; set; }
-
-        private string imagePath { get; set; }
-
-
 
         private BitmapImage _bitmapImage;
         public BitmapImage bitmapImage
@@ -64,71 +54,58 @@ namespace AnimatedColorfulMenu.ViewModel
             }
         }
 
-        SanPham sanphamWD;
-        public ICommand setupSanPham { get; set; }
+        private string imagePath { get; set; }
 
-        public SanPhamViewModel()
+
+
+        private UpdateProduct updateProduct;
+        public UpdateProductViewModel()
         {
-
-            ResetFormInput();
-            string path = AppDomain.CurrentDomain.BaseDirectory + @"quanlyquanao-firebase-adminsdk-x253q-2787a0673d.json";
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
-            setupSanPham = new RelayCommand<SanPham>((p) => { return true; }, (p) => sanphamWD = p);
-            themProduct = new RelayCommand<SanPham>((p) => { return checkValidInput(p); }, (p) => { addProduct(p); });
+            themProduct = new RelayCommand<Object>((p) => { return checkValidInput(); }, (p) => { addProduct(); });
             chonHinhAnh = new RelayCommand<Image>((p) => { return true; }, (p) => { chooseImage(p); });
-            xoaProduct = new RelayCommand<Product>((p) =>
+            
+            
+        }
+
+        Product product1;
+        public void SetupUpdate(Product product)
+        {
+            product1 = product;
+            NameProduct = product.productName;
+            PriceProduct = product.price.ToString();
+            bitmapImage = new BitmapImage(new Uri(product.imageUrl));
+            updateProduct = new UpdateProduct();
+            updateProduct.ShowDialog();
+        }
+
+        private bool checkValidInput()
+        {
+            return NameProduct.Length > 0 && PriceProduct.Length > 0 && (imagePath != null || bitmapImage != null);
+        }
+
+        private async void addProduct()
+        {
+            string id = product1.productID.ToString();
+            string link = "";
+            if (imagePath != null)
             {
-                return p != null;
-            }, (p) =>
-            {
-                RemoveProduct(p);
-            });
-
-            suaProduct = new RelayCommand<Product>((p) => { return true; }, (p) => {
-                EditProduct(p);
-            });
-
+                link = await upLoadimageAsync(id);
+            }
+                
+            Product productz = new Product() { productID = product1.productID, productName = NameProduct, price = float.Parse(PriceProduct), imageUrl = imagePath != null ? link : product1.imageUrl };
+            iaddProduct(productz);
+            updateProduct.Close();
         }
 
-
-        private void EditProduct(Product e)
+        public void iaddProduct(Product product)
         {
-            UpdateProductViewModel updateProductViewModel = sanphamWD.updateVMT.DataContext as UpdateProductViewModel;
-            updateProductViewModel.SetupUpdate(e);
-            loadProduct();
+            var result = DataProvider.Ins.DB.Products.Where(i => i.productID == product.productID).ToList()[0];
+            result.imageUrl = product.imageUrl;
+            result.price = product.price;
+            result.productName = product.productName;
+            DataProvider.Ins.DB.SaveChanges();
         }
 
-        private void RemoveProduct(Product e)
-        {
-            deleteProduct(e);
-        }
-        private void ResetFormInput()
-        {
-            BitmapImage bitmap = new BitmapImage();
-            bitmap.BeginInit();
-            bitmap.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
-            bitmap.CacheOption = BitmapCacheOption.OnLoad;
-            bitmap.UriSource = new Uri("pack://application:,,,/Assets/school-bag.png");
-            bitmap.EndInit();
-            bitmapImage = bitmap.Clone();
-            PriceProduct = null;
-            NameProduct = null;
-        }
-
-        private bool checkValidInput(SanPham sanPham)
-        {
-            if (sanPham == null) return false;
-            return sanPham.productName.Text.Length > 0 && sanPham.productPrice.Text.Length > 0 && imagePath != null;
-        }
-
-        private async void addProduct(SanPham sanPham)
-        {
-            string id = Guid.NewGuid().ToString("N");
-            string link = await upLoadimageAsync(id);
-            Product product = new Product() { productName = NameProduct, price = float.Parse(PriceProduct), imageUrl = link };
-            addProduct(product);
-            ResetFormInput();
-        }
 
         private async Task<string> upLoadimageAsync(string pid)
         {
